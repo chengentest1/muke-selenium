a3e19bec5473fac66e32c4d7cb2fc545b975e308
chengentest1 <chengen_cg123@126.com>
2018-11-20 21:52:23 +0800

    更新

--- case/first_case.py
+++ case/first_case.py
@@ -3,34 +3,45 @@ import HTMLTestRunner
 from selenium import webdriver
 import unittest,os,time
 class FirstCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_name = "C:/Users/cheng/PycharmProjects/muke-selenium/report/test_login_email_error.png"
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
     def setUp(self):
         self.driver=webdriver.Chrome()
         self.driver.get('http://www.5itest.cn/register')
         self.login=RegisterBusiness(self.driver)
     def tearDown(self):
-        time.sleep(3)
+        time.sleep(2)
+        for method_name,error in self._outcome.errors:
+            if error:
+                case_name = self._testMethodName
+                file_path = os.path.join(os.path.dirname(os.getcwd())+'/report/'+case_name+'.png')
+                self.driver.save_screenshot(file_path)
         self.driver.close()
     def test_login_email_error(self):
-        email_error=self.login.login_email_error('34', 'user111','111111','test1')
+        email_error=self.login.login_email_error('34', 'user111','111111',self.file_name)
         self.assertFalse(email_error,'此条case执行失败')
         # if email_error==True:
         #     print('注册成功了，此条case执行失败')
     @unittest.skip('跳过')
     def test_login_username_error(self):
-        username_error = self.login.login_name_error('111@qq.com','ss','111111','test2')
+        username_error = self.login.login_name_error('111@qq.com','ss','111111',self.file_name)
         if username_error ==True:
 
             print('此条case执行失败')
 
     @unittest.skip('跳过')
     def test_login_code_error(self):
-        password_error = self.login.login_code_error('34@qq.com', 'user111','111111','test1')
+        password_error = self.login.login_code_error('34@qq.com', 'user111','111111',self.file_name)
         if password_error == True:
             print('此条case执行失败')
 
     @unittest.skip('跳过')
     def test_login_success(self):
-        succes =self.login.user_base('34@qq.com', 'user111','111111','test1')
+        succes =self.login.user_base('34@qq.com', 'user111','111111',self.file_name)
         if self.login.register_succes()==True:
             print('注册成功')
 
--- handle/register_handle.py
+++ handle/register_handle.py
@@ -1,9 +1,11 @@
 from page.register_page import RegisterPage
+from util.get_code import GetCode
 
 
 class RegisterHandle(object):
     def __init__(self,driver):
-        self.register_p=RegisterPage(driver)
+        self.driver=driver
+        self.register_p=RegisterPage(self.driver)
     def send_user_email(self,email):
         self.register_p.get_email_element().send_keys(email)
     def send_user_name(self,name):
@@ -11,19 +13,24 @@ class RegisterHandle(object):
     def send_user_password(self,password):
         self.register_p.get_password_element().send_keys(password)
     #输入验证码
-    def send_user_code(self,code):
+    def send_user_code(self,file_name):
+        get_code_text = GetCode(self.driver)
+        code=get_code_text.code_online(file_name)
         self.register_p.get_code_element().send_keys(code)
     #获取文字
     def get_user_text(self,info,user_info):
-        if info=='email_error':
-           text = self.register_p.get_email_error_element().text
-           
-        elif info=='name_error':
-            text =self.register_p.get_name_error_element().get_attribute('value')
-        elif info=='password_error':
-            text =self.register_p.get_password_error_element().get_attribute('value')
-        else:
-            text =self.register_p.get_code_error_element().get_attribute('value')
+        try:
+            if info=='email_error':
+               text = self.register_p.get_email_error_element().text
+
+            elif info=='name_error':
+                text =self.register_p.get_name_error_element().text
+            elif info=='password_error':
+                text =self.register_p.get_password_error_element().text
+            else:
+                text =self.register_p.get_code_error_element().text
+        except:
+            text=None
         return text
     #点击注册按钮
     def click_register_button(self):
